dce46f1ab672badec7a68dbf4ba63349bde1db58
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-22 15:01:19 +0200

    Update test script Procedures/Startup.py: -# force 14V on "VCC" pin wrt GND1+ | +# apply vddio @ 1.8v wrt "GND1m" | +# force all "VCC"'s @14V wrt "GND1m"  | +VFORCE(signal="CD/DIAG",reference="GND1m",value=14)

--- Procedures/Startup.py
+++ Procedures/Startup.py
@@ -1,5 +1,7 @@
 # import the dfttool functionalities
 print(f'... Startup Procedure ......')
 from dfttools import VFORCE
-# force 14V on "VCC" pin wrt GND1+
+# apply vddio @ 1.8v wrt "GND1m"
+# force all "VCC"'s @14V wrt "GND1m" 
 VFORCE(signal="VCC",reference="GND1m",value=14)
+VFORCE(signal="CD/DIAG",reference="GND1m",value=14)
