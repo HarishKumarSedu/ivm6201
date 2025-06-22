87a21ac5278c9d7f776de5a627f54f2fb0e77ca4
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-22 14:42:26 +0200

    Initial commit

---
+++ Procedures/Startup.py
@@ -0,0 +1,5 @@
+# import the dfttool functionalities
+print(f'... Startup Procedure ......')
+from dfttools import VFORCE
+# force 14V on "VCC" pin wrt GND1+
+VFORCE(signal="VCC",reference=GND1-,value=14)
