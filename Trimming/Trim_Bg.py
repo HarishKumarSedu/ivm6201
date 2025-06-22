4347e1f13d31a87e909782d7ae2d85bbf31ff9c9
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-22 15:39:19 +0200

    Update test script Trimming/Trim_Bg.py: -print(bit_width) | -step_size = 0.02 | +step_size = typical_value / bit_width | +print(step_size)

--- Trimming/Trim_Bg.py
+++ Trimming/Trim_Bg.py
@@ -14,9 +14,9 @@ low_value = 1.18
 typical_value = 1.242
 high_value = 1.3
 bit_width = {'fieldname': 'ref_vbg_trim', 'length': 4, 'registers': [{'REG': '0xC1', 'POS': 4, 'RegisterName': 'OTP register 129', 'RegisterLength': 8, 'Name': 'ref_vbg_trim[3:0]', 'Mask': '0xF0', 'Length': 4, 'FieldMSB': 3, 'FieldLSB': 0, 'Attribute': 'NNNNNNNN', 'Default': '00', 'User': '00000000', 'Clocking': 'FRO', 'Reset': 'C', 'PageName': 'PAG1'}]}.get('length')
-print(bit_width)
 # Step size
-step_size = 0.02
+step_size = typical_value / bit_width
+print(step_size)
 
 # Number of steps width of the field / bits
 num_steps = 2**4  # 4-bit
