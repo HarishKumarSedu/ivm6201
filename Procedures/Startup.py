f611a8c9b4227cd2bf602d04fe5af75e7fa6da03
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-22 14:48:45 +0200

    Update test script Procedures/Startup.py: -VFORCE(signal="VCC",reference="GND1p",value=14) | +VFORCE(signal="VCC",reference="GND1m",value=14)

--- Procedures/Startup.py
+++ Procedures/Startup.py
@@ -2,4 +2,4 @@
 print(f'... Startup Procedure ......')
 from dfttools import VFORCE
 # force 14V on "VCC" pin wrt GND1+
-VFORCE(signal="VCC",reference="GND1p",value=14)
+VFORCE(signal="VCC",reference="GND1m",value=14)
