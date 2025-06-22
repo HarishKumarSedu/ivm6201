51b712cc287cb3ce8b8414569767a83fdb0329a5
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-22 16:38:20 +0200

    Update test script Procedures/Startup_5p0V.py: -from dfttools import VFORCE | +from dfttools import * | +Test_name = 'Startup_5p0V' | +print(f'.... {Test_name} Procedure........')

--- Procedures/Startup_5p0V.py
+++ Procedures/Startup_5p0V.py
@@ -1,5 +1,7 @@
 # import the dfttool functionalities
-from dfttools import VFORCE
+from dfttools import *
+Test_name = 'Startup_5p0V'
+print(f'.... {Test_name} Procedure........')
 # apply vddio @ 1.8v wrt "GND1m"
 # force all "VCC"'s @14V wrt "GND1m" 
 VFORCE(signal="VCC",reference="GND1m",value=5.0)
