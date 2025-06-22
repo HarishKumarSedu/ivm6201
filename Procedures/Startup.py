704d0c345eebac497c5281838270cc1257a2dac7
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-06-22 15:03:08 +0200

    Update test script Procedures/Startup.py: -VFORCE(signal="CD/DIAG",reference="GND1m",value=14) | +VFORCE(signal="CD/DIAG",reference="GND1m",value=1.8)

--- Procedures/Startup.py
+++ Procedures/Startup.py
@@ -4,4 +4,4 @@ from dfttools import VFORCE
 # apply vddio @ 1.8v wrt "GND1m"
 # force all "VCC"'s @14V wrt "GND1m" 
 VFORCE(signal="VCC",reference="GND1m",value=14)
-VFORCE(signal="CD/DIAG",reference="GND1m",value=14)
+VFORCE(signal="CD/DIAG",reference="GND1m",value=1.8)
